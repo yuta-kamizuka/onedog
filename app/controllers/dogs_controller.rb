02a3ad73cd90707854d6ddf16d256a298b3cedaf
yuta-kamizuka <cybor_ggg@icloud.com>
2020-12-24 21:24:21 +0900

    登録データ保存実装

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -5,4 +5,25 @@ class DogsController < ApplicationController
   def new
     @dog = Dog.new
   end
+
+  def create
+    @dog = Dog.new(dog_params)
+    if
+      @dog.save
+      redirect_to root_path
+    else
+    end
+  end
+
+  private
+  def dog_params
+    params.require(:dog).permit(
+      :name,
+      :introduction,
+      :dog_breed,
+      :age,
+      :sex,
+      :condition
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  resources :dogs, only: [:index, :new]
+  resources :dogs, only: [:index, :new, :create]
 end
