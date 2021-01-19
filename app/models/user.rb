b77a1c09c40db77ea2016b9391af588209c0c254
yuta-kamizuka <cybor_ggg@icloud.com>
2021-01-19 17:32:14 +0900

    userエラーハンドリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: {maximum: 6 }
   validates :first_name, presence: true, format: {
     with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
     message: "は全角入力をお願いします"
   }
-
   validates :family_name, presence: true, format: {
     with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
     message: "は全角入力をお願いします"
   }
+  validates :first_name_kana, presence: true, format: {
+    with: /\A[\p{katakana}\p{blank}ー－]+\z/,
+    message: 'は全角カナで入力して下さい。'
+   }
+  validates :family_name_kana, presence: true, format: {
+    with: /\A[\p{katakana}\p{blank}ー－]+\z/,
+    message: 'は全角カナで入力して下さい。'
+    }
 
 
   validates :phone, presence: true
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  
+  factory :user do
+    nickname              {"hoge"}
+    email                 {"kkk@gmail.com"}
+    password              {"00000000"}
+    password_confirmation {"00000000"}
+    first_name            {"太郎"}
+    family_name           {"日本"}
+    first_name_kana       {"タロウ"}
+    family_name_kana      {"ニホン"}
+    phone                 {"08000000000"}
+  end
+end
