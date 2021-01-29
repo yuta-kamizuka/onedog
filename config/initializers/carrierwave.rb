6cf78b4a254ed6b9503252acc1209378d60ee2d2
yuta-kamizuka <cybor_ggg@icloud.com>
2021-01-30 05:33:29 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'onedog6288'
+  config.fog_directory = 'onedog6288'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/onedog6288'
 end
